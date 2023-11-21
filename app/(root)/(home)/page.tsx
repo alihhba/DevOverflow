c63211eb62469e7a780e8e06e6ede9fc7d92d811
alihhba <ahbhbhba@gmail.com>
2023-11-21 22:30:13 +0330

    routing & clerk auth

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,15 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      Home
+      <div>
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
